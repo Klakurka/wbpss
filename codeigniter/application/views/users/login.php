f89ca25d899c98966436bbeb1e66cdf5cbcc6b42
David <Klakurka@gmail.com>
2013-07-18 14:35:50 -0700

    Added HTML5 Required Tags

--- codeigniter/application/views/users/login.php
+++ codeigniter/application/views/users/login.php
@@ -3,10 +3,10 @@ echo validation_errors();
 echo form_open('users/login')."\n";
 
 echo form_label('User name', 'user_name');
-echo form_input(array('name' => 'user_name', 'autofocus' => 'autofocus'))."\n";
+echo form_input(array('name' => 'user_name', 'autofocus' => 'autofocus', 'required' => 'required'))."\n";
 
 echo form_label('Password','password');
-echo form_password('password')."\n";
+echo form_password(array('name' => 'password', 'required' => 'required'))."\n";
 
 echo form_submit('submit', 'Login')."\n";
 
